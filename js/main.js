2e05282d261b34289c93bc8a6ed2ac0a66f9e8c1
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2022-09-14 20:58:49 +0500

    make workable modal open close

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let elAppBtns = document.querySelectorAll(".app__btn");
+const elAppBtns = document.querySelectorAll(".app__btn");
 
 elAppBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -9,4 +9,15 @@ elAppBtns.forEach((btn) => {
       btn.classList.add("active");
     }
   });
+});
+
+const elAppPlusBtn = document.querySelector(".app__plus-btn");
+const elModalCloseBtn = document.querySelector(".form__close-btn");
+const elModal = document.querySelector(".modal");
+
+elAppPlusBtn.addEventListener("click", () => {
+  elModal.classList.add("active");
+});
+elModalCloseBtn.addEventListener("click", () => {
+  elModal.classList.remove("active");
 });
